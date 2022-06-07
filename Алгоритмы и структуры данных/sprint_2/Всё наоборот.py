96266452b4ba2e614ee4c59b1f040fb7b574f87e
Сергей Жарковский <sergeizharkovsky@yandex.ru>
2022-06-07 15:50:38 +0300

    Добавил новые задачи

---
+++ Алгоритмы и структуры данных/sprint_2/Всё наоборот.py
@@ -0,0 +1,35 @@
+class DoubleConnectedNode:
+    def __init__(self, value, next=None, prev=None):
+        self.value = value
+        self.next = next
+        self.prev = prev
+
+
+def solution(node):
+    while node.next is not None:
+        node = node.next
+    new_node = node
+    while node.prev is not None:
+        head = node.prev
+        node.prev = node.next
+        node.next = head
+        node = head
+    return new_node
+
+
+def test():
+    node3 = DoubleConnectedNode("node3")
+    node2 = DoubleConnectedNode("node2")
+    node1 = DoubleConnectedNode("node1")
+    node0 = DoubleConnectedNode("node0")
+
+    node0.next = node1
+
+    node1.prev = node0
+    node1.next = node2
+
+    node2.prev = node1
+    node2.next = node3
+
+    node3.prev = node2
+    new_head = solution(node0)
---
+++ Алгоритмы и структуры данных/sprint_2/Список дел.py
@@ -0,0 +1,12 @@
+def solution(node):
+    while node is not None:
+        print(node.value)
+        node = node.next_item
+
+
+def test():
+    node3 = Node("node3", None)
+    node2 = Node("node2", node3)
+    node1 = Node("node1", node2)
+    node0 = Node("node0", node1)
+    solution(node0)
